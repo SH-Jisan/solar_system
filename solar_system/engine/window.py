c20c3c0520ac3dc8673687b521dbf613986de622
SH <shzisun123@gmail.com>
2026-02-02 03:26:36 +0600

    color fix

--- solar_system/engine/window.py
+++ solar_system/engine/window.py
@@ -6,6 +6,7 @@ def setup_lighting():
     glEnable(GL_LIGHTING)
     glEnable(GL_LIGHT0)
     glEnable(GL_COLOR_MATERIAL)
+    glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
 
     glLightfv(GL_LIGHT0, GL_POSITION, (1, 1, 1, 1))
     glLightfv(GL_LIGHT0, GL_DIFFUSE, (1, 1, 1, 1))
--- solar_system/geometry/sphere.py
+++ solar_system/geometry/sphere.py
@@ -3,8 +3,6 @@ from OpenGL.GL import *
 
 def draw_sphere(radius=2, lat_div=30, lon_div=60):
 
-    glColor3f(1.0, 0.6, 0.0)  # Sun color (orange)
-
     for i in range(lat_div):
         theta1 = math.pi * i / lat_div
         theta2 = math.pi * (i + 1) / lat_div
@@ -14,15 +12,28 @@ def draw_sphere(radius=2, lat_div=30, lon_div=60):
         for j in range(lon_div + 1):
             phi = 2 * math.pi * j / lon_div
 
+            # ----- first ring point -----
             x1 = radius * math.sin(theta1) * math.cos(phi)
             y1 = radius * math.sin(theta1) * math.sin(phi)
             z1 = radius * math.cos(theta1)
 
+            nx1 = x1 / radius
+            ny1 = y1 / radius
+            nz1 = z1 / radius
+
+            glNormal3f(nx1, ny1, nz1)
+            glVertex3f(x1, y1, z1)
+
+            # ----- second ring point -----
             x2 = radius * math.sin(theta2) * math.cos(phi)
             y2 = radius * math.sin(theta2) * math.sin(phi)
             z2 = radius * math.cos(theta2)
 
-            glVertex3f(x1, y1, z1)
+            nx2 = x2 / radius
+            ny2 = y2 / radius
+            nz2 = z2 / radius
+
+            glNormal3f(nx2, ny2, nz2)
             glVertex3f(x2, y2, z2)
 
         glEnd()
